ddb7128f52ae11e4c47e128f4e27a71506461de3
cttsrk <cttsrk@gmail.com>
2021-12-28 12:56:18 +0100

    more pedantic cleanups in 2015/day07

--- 2015/day07/src/main.rs
+++ 2015/day07/src/main.rs
@@ -2,12 +2,6 @@
 //
 // Learning exercise by github.com/cttsrk
 //
-// Part1: Find out the output of wire "a" in the provided circuit.
-//
-// Part2: Pin wire "b" to the value of the output from part1 (meaning no
-// feedback, just copy out the value once) and find out the new output of wire
-// "a".
-//
 // This one was far enough outside my experience that the learning curve was
 // quite steep. I made detours into parsing/lexing, the borrow checker,
 // hashmaps, destructuring, enums, etc.
@@ -15,7 +9,7 @@
 // I think maybe this puzzle was designed for recursive solving so I went for
 // that and had a nice aha moment with the placement of the base case.
 //
-// Por part 2, it took some thinking to figure out how to clear the cache
+// For part 2, it took some thinking to figure out how to clear the cache
 // without another pass over the input file. I didn't find a way to iterate over
 // the hashmap and modify its entries at the same time, so I ended up saving a
 // separate vector of the key names from the first pass.
@@ -50,45 +44,47 @@ fn parse(circuit: &str) -> (Vec<&str>, HashMap<&str, Instr>) {
 
 fn solve(wire: &str, mut map: &mut HashMap<&str, Instr>) -> u16 {
     // Checking for the direct value base case here seems simpler than dealing
-    // with multi-type fields in the Instr struct.
+    // with multi-type fields in the Instr struct and op match.
     if let Ok(value) = wire.parse::<u16>() { return value; }
 
     // Check if this wire is already solved. An else branch could be used here
     // to guard against circular dependencies by checking for a seen bool.
     if let Some(result) = map[wire].result { return result; }
 
-    let (mut val1, mut val2) = (0, 0);
-    if let Some(val) = map[wire].in1 { val1 = solve(val, &mut map); }
-    if let Some(val) = map[wire].in2 { val2 = solve(val, &mut map); }
+    // Recursively solve any inputs that are not `None`
+    let v1 = if let Some(v) = map[wire].in1 { solve(v, &mut map) } else { 0 };
+    let v2 = if let Some(v) = map[wire].in2 { solve(v, &mut map) } else { 0 };
 
+    // Perform the logic operation that feeds this wire. No operator implies a
+    // direct value / forwarding from another wire.
     let result = match map[wire].op {
-        Some("AND")    => val1 & val2,
-        Some("OR")     => val1 | val2,
-        Some("NOT")    => ! val2,
-        Some("RSHIFT") => val1 >> val2,
-        Some("LSHIFT") => val1 << val2,
-        None           => val2,
+        Some("AND")    => v1 & v2,
+        Some("OR")     => v1 | v2,
+        Some("NOT")    => ! v2,
+        Some("RSHIFT") => v1 >> v2,
+        Some("LSHIFT") => v1 << v2,
+        None           => v2,
                      _ => panic!("Invalid input")
     };
 
-    if let Some(wire) = map.get_mut(wire) { wire.result = Some(result); }
+    // This wire has been solved for the first time, cache the result for re-use
+    // when other paths through the circuit reach this wire.
+    if let Some(w) = map.get_mut(wire) { w.result = Some(result); }
 
     result
 }
 
 fn main() {
-    let circuit = include_str!("../circuit.txt");
-    let (list, mut map) = parse(circuit);
-
+    // Parse input, solve part 1
+    let (list, mut map) = parse(include_str!("../circuit.txt"));
     let (key, pin) = ("a", "b");
     let part1 = solve(key, &mut map);
 
-    // Clear saved results
-    for w in list { if let Some(wire) = map.get_mut(w) { wire.result = None; } }
-
-    if let Some(wire) = map.get_mut(pin) { wire.result = Some(part1); }
+    // Clear cache, pin wire "b", solve part 2
+    for wire in list { if let Some(w) = map.get_mut(wire) { w.result = None; } }
+    if let Some(w) = map.get_mut(pin) { w.result = Some(part1); }
     let part2 = solve(key, &mut map);
 
-    println!("Part 1: Wire {} outputs {}", key, part1);
-    println!("Part 2: Pinning {} to {}, {} outputs {}", pin, part1, key, part2);
+    println!("Part 1: Wire 'a' outputs {}", part1);
+    println!("Part 2: Pinning wire 'b' to {}, 'a' outputs {}.", part1, part2);
 }
